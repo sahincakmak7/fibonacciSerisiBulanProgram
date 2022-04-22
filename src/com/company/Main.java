4c4f7ccff0ea936b35714332d22d0795ace8cad7
Şahin <sahincakmak7@gmail.com>
2022-04-22 21:00:05 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    static int fib(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int num;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz :");
+        num = input.nextInt();
+        System.out.println(num + ". serinin sonucu :" + fib(num));
+    }
+}
+
+
+
